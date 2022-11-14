b9b4ed0d4e5dbb53a7d4973f45f2c87266ba452c
Nitin Soni <nitinsoni3007@gmail.com>
2022-11-15 01:00:50 +0530

    initial code commit

---
+++ FitnessGuide/FitnessGuide/FitnessGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessGuideApp.swift
+//  FitnessGuide
+//
+//  Created by Nitin Soni on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessGuide/FitnessGuideTests/FitnessGuideTests.swift
@@ -0,0 +1,26 @@
+//
+//  FitnessGuideTests.swift
+//  FitnessGuideTests
+//
+//  Created by Nitin Soni on 15/11/22.
+//
+
+import XCTest
+@testable import FitnessGuide
+
+final class FitnessGuideTests: XCTestCase {
+    var sut: ContentViewModel?
+    override func setUpWithError() throws {
+        sut = ContentViewModel()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func testFindExpectedWeight() throws {
+        let expectedWt = sut?.findExpectedWeight(age: 40, height: 172)
+        XCTAssertNotNil(expectedWt)
+    }
+
+}
